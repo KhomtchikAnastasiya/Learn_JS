d0ecc078b036e79e65ec1b6d2a5e9b5eba4bb64a
Lawrik-nastik <lawrik.nastik@gmail.com>
2023-01-08 23:47:25 +0300

    test1

--- Homework_23_DYN_FORM/DYN_FORM.js
+++ Homework_23_DYN_FORM/DYN_FORM.js
@@ -4,57 +4,87 @@ function form(formName, mass) {
     var frm=document.forms[formName];
    
     for (let i=0; i<mass.length; i++ ) {
-        if (mass[i]==='label') {
-            let newTagElementLabel=document.createElement('label');
-            let newTextElementLabel=document.createTextNode(mass[i].label);
-            newTagElementLabel.appendChild(newTagElementBr);
-            frm.appendChild(newTagElementLabel);
-        }
 
+        
+      
+        
         if (mass[i].kind==='submit') {
-            var newTagElementButton=document.createElement('button');
-            var newTextElementCaption=document.createTextNode(mass[i].caption);
-            newTagElementButton.appendChild(newTextElementCaption);
-            frm.appendChild(newTagElementButton);  
-        }
-        else if(mass[i].kind==='combo') {
+          var newTagElementButton=document.createElement('button');
+          var newTextElementCaption=document.createTextNode(mass[i].caption);
+          newTagElementButton.appendChild(newTextElementCaption);
+          frm.appendChild(newTagElementButton);  
+       }
+        
+       
+       if (mass[i].label) {
+
+          let newTagElementLabel=document.createElement('label');
+          let newTextElementLabel=document.createTextNode(mass[i].label);
+          newTagElementLabel.appendChild(newTextElementLabel);
+          frm.appendChild(newTagElementLabel);
+    
+          if(mass[i].kind==='combo') {
            
             var newTagElementSelect=document.createElement('select');
-            newTagElementSelect.appendChild(newTagElementLabel);
+            newTagElementSelect.setAttribute('name', mass[i].name);
+            newTagElementLabel.appendChild(newTagElementSelect);
             for (let n=0; n<mass[i].variants.length; n++ ) {
                 var newTagElementOption=document.createElement('option');
-                newTagElementOption.setAttribute('value',mass[i].variants[n].value);
+                newTagElementOption.setAttribute('value', mass[i].variants[n].value);
                 var newTextElementOption=document.createTextNode(mass[i].variants[n].text);
                 newTagElementOption.appendChild(newTextElementOption);
+                newTagElementSelect.appendChild(newTagElementOption);
+            }   
+          }
+
+          else if(mass[i].kind==='memo') {
+
+            var newTagElementTextarea=document.createElement('textarea');
+            newTagElementTextarea.setAttribute('name', mass[i].name);
+            newTagElementLabel.appendChild(newTagElementTextarea);
+           }
+          
+          else if(mass[i].kind==='radio') {
+
+            for (let m=0; m<mass[i].variants.length; m++ ) {
+
+            var newTagElementLabelInput=document.createElement('label');
+            var newTextElementLabelInput=document.createTextNode(mass[i].variants[m].text);
+            newTagElementLabelInput.appendChild(newTextElementLabelInput);
+            newTagElementLabel.appendChild(newTagElementLabelInput);
+
+            var newTagElementInput=document.createElement('input');
+            newTagElementInput.setAttribute('name', mass[i].name);
+            newTagElementInput.setAttribute('type','radio');
+            newTagElementInput.setAttribute('value', mass[i].variants[m].value);
+            
+            newTagElementLabelInput.appendChild(newTagElementInput);
             }
-              
-        }
-        else if(mass[i].kind==='memo') {
+          }
+          
+          else {          
+          
+          var newTagElementInput=document.createElement('input');
+          newTagElementInput.setAttribute('name', mass[i].name);
+          newTagElementLabel.appendChild(newTagElementInput);
+
+            if (mass[i].kind==='longtext'||mass[i].kind==='shorttext'||mass[i].kind==='number')
+            newTagElementInput.setAttribute('type','text')
+            else if(mass[i].kind==='check') {
             newTagElementInput.setAttribute('type','checkbox');
             newTagElementInput.setAttribute('checked','true');
             }
-        
-        else {
-        
-        
-        var newTagElementInput=document.createElement('input');
-        newTagElementLabel.appendChild(newTagElementInput);
-        var newTagElementBr=document.createElement('br');
-        newTagElementLabel.appendChild(newTagElementBr);
-        frm.appendChild(newTagElementLabel);}
-
-        if (mass[i].kind==='longtext'||mass[i].kind==='shorttext')
-        newTagElementInput.setAttribute('type','text')
-        else if(mass[i].kind==='check') {
-        newTagElementInput.setAttribute('type','checkbox');
-        newTagElementInput.setAttribute('checked','true');
+          }
         }
-        
+
+        var newTagElementBr=document.createElement('br');
+        frm.appendChild(newTagElementBr);
 
 
     }
-   return frm;
-}
+    return frm;
+
+  }
 
 var formDef1=
 [
@@ -80,5 +110,8 @@ var formDef2=
   {caption:'Зарегистрироваться',kind:'submit'},
 ];
 
-form('form1', formDef1);
+form('Form1', formDef1);
+
+form('Form2', formDef2);
+
 
